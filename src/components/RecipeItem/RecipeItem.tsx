61408836d167e30b062a5a91de40cd224d85281f
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-04-10 13:48:11 +0530

    created about Recipe feature

--- src/components/RecipeItem/RecipeItem.tsx
+++ src/components/RecipeItem/RecipeItem.tsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardContent, Typography, IconButton } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Typography,
+  IconButton,
+  Button,
+} from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { assets } from "../../assets/assets";
+
 import "./RecipeItem.css";
 
 interface RecipeItemProps {
@@ -9,7 +16,7 @@ interface RecipeItemProps {
   recipeTitle: string;
   image: string;
   ingredients: string[];
-  cookingTime:string;
+  cookingTime: string;
   instructions: string;
   isFavoritePage?: boolean;
   onRemoveFavorite?: () => void;
@@ -28,6 +35,7 @@ const RecipeItem: React.FC<RecipeItemProps> = ({
   onClick,
 }) => {
   const [isFavorite, setIsFavorite] = useState(false);
+  const [recipeDetails, setRecipeDetails] = useState<any>(null);
 
   useEffect(() => {
     const existing = localStorage.getItem(id);
@@ -47,12 +55,19 @@ const RecipeItem: React.FC<RecipeItemProps> = ({
     }
   };
 
-  const handleIconClick = () => {
+  const handleIconClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation(); 
     if (isFavoritePage && onRemoveFavorite) {
       onRemoveFavorite();
     } else {
       handleFavoriteClick();
     }
+  };
+
+  const fetchRecipeDetails = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation(); 
+    // Now you can add logic to fetch recipe details if needed
+    console.log("Fetching details for the recipe...");
   };
 
   return (
@@ -64,12 +79,29 @@ const RecipeItem: React.FC<RecipeItemProps> = ({
         borderRadius: 2,
         boxShadow: 2,
         backgroundColor: "#f6e1d2",
+        position: "relative", // Add this to make the favorite icon position absolute
       }}
       onClick={onClick}
     >
       <div className="recipe-item-img-container">
         <img src={image} alt={recipeTitle} className="recipe-item-image" />
       </div>
+
+      {/* Favorite Icon in Top Right Corner */}
+      <IconButton
+        onClick={handleIconClick}
+        sx={{
+          position: "absolute",
+          top: 10,
+          right: 10,
+          padding: 1,
+          backgroundColor: "white", // Corrected to "backgroundColor"
+          borderRadius: "50%", // To make it a circle, use borderRadius: "50%"
+          color: isFavorite || isFavoritePage ? "red" : "gray",
+        }}
+      >
+        <FavoriteIcon />
+      </IconButton>
 
       <CardContent className="product-item-info">
         <div className="product-item-name-rating">
@@ -112,11 +144,24 @@ const RecipeItem: React.FC<RecipeItemProps> = ({
           )}
         </ul>
 
-        <IconButton className="favorite-icon" onClick={handleIconClick}>
-          <FavoriteIcon
-            sx={{ color: isFavorite || isFavoritePage ? "red" : "gray" }}
-          />
-        </IconButton>
+        {/* About This Recipe Button */}
+        <Button
+          sx={{
+            marginTop: 2,
+            fontFamily: "Montserrat, sans-serif",
+            fontWeight: "bold",
+            backgroundColor: "red",
+            color: "white",
+            width: "100%",
+            fontSize: "12px",
+            "&:hover": {
+              border: "2px solid white",
+            },
+          }}
+          onClick={fetchRecipeDetails} // Fetch recipe details on button click
+        >
+          About This Recipe
+        </Button>
       </CardContent>
     </Card>
   );
